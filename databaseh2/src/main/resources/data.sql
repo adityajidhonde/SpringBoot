afbe8a60488268243dc705748e6763b3443f1a26
Aditya Dhonde <adityadhonde@adityas-mbp.mynetworksettings.com>
2024-10-03 21:21:46 -0400

    Done

--- databaseh2/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into alien (id,name,tech) values (101,'Kiran','Bloackchain');
-insert into alien (id,name,tech) values (102,'Aditya','Java');
-insert into alien (id,name,tech) values (103,'prajwal','Cpp');
--- databaseh2/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE alien (
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-    tech VARCHAR(20)
-);
